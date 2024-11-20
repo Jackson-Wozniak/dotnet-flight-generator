d94f66481ddad1528e9a478fbb6ee32448573c2f
Jackson Wozniak <jjw30@comcast.net>
2024-11-19 23:03:16 -0500

    initial files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using backend.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHostedService<AirportConfiguration>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
---
+++ src/Configuration/AirportConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace backend.Configuration;
+
+public class AirportConfiguration : IHostedService
+{
+    private static readonly string AIRPORT_PATH = "App_Data/Airports.csv";
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        InitializeAirports();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private static void InitializeAirports()
+    {
+        // using (var reader = new StreamReader(AIRPORT_PATH))
+        // using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        // {
+        //     var records = csv.GetRecords<dynamic>().ToList();
+        //
+        //     foreach (var record in records)
+        //     {
+        //         Console.WriteLine(record);
+        //     }
+        // }
+    }
+}
