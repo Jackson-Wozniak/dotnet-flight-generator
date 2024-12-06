4d04f402f4a72542f48f5d06bb9efeec6161b924
Jackson Wozniak <jjw30@comcast.net>
2024-12-06 11:34:36 -0500

    add test framework, and add a Flight.cs test

---
+++ tests/Models/FlightTests.cs
@@ -0,0 +1,25 @@
+﻿using backend.Entities;
+using backend.Models;
+using Xunit;
+
+namespace tests.Data;
+
+public class FlightTests
+{
+    private const double KmhtToKjfkDistance = 198.8;
+    private static readonly Airport Kmht = Airport.Create("KHMT", "Manchester Boston Regional", 
+        42.9297, 71.4352, "NA", "USA");
+    private static readonly Airport Kjfk = Airport.Create("KFJK", "John F. Kennedy International", 
+        40.6446, 73.7797, "NA", "USA");
+    
+    
+    [Fact]
+    public void FlightDistanceCorrect()
+    {
+        var kmhtToKjfk = Flight.Create(Kmht, Kjfk);
+        var kjfkToKmht = Flight.Create(Kjfk, Kmht);
+        
+        Assert.Equal(kmhtToKjfk.DistanceMiles, kjfkToKmht.DistanceMiles);
+        Assert.Equal(KmhtToKjfkDistance, kmhtToKjfk.DistanceMiles);
+    }
+}
